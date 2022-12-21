76376a16ee87c784c622b95872e604d5c50f2921
Joel Holsteen <grindlemire@gmail.com>
2022-12-21 15:30:34 -0700

    update test cmd

--- cmd/go.mod
+++
@@ -1,7 +0,0 @@
-module github.com/grindlemire/go-lucene/cmd
-
-go 1.18
-
-require github.com/grindlemire/go-lucene v0.0.0-20221213071633-f37ef5100aff
-
-replace github.com/grindlemire/go-lucene => ../
--- cmd/test.go
+++ cmd/test.go
@@ -8,6 +8,10 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Please provide a lucene query\n")
+		os.Exit(1)
+	}
 	e, err := lucene.Parse(os.Args[1])
 	if err != nil {
 		fmt.Printf("Error parsing: %s\n", err)
