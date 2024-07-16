3dea7db7ec34ae2479c842935056390fe2f34100
grindlemire <grindlemire@gmail.com>
2024-07-16 15:39:28 -0600

    add option for setting the default field

--- fuzz/fuzz_test.go
+++ fuzz/fuzz_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/grindlemire/go-lucene"
 	"github.com/grindlemire/go-lucene/pkg/driver"
+	"github.com/grindlemire/go-lucene/pkg/lucene/expr"
 	pg_query "github.com/pganalyze/pg_query_go/v4"
 )
 
@@ -34,28 +35,41 @@ func FuzzPostgresDriver(f *testing.F) {
 			return
 		}
 
-		f, err := driver.NewPostgresDriver().Render(e)
+		validateRender(t, e)
+
+		// Test the default field option.
+		e, err = lucene.Parse(in, lucene.WithDefaultField("default"))
 		if err != nil {
-			// Ignore errors that are expected.
-			if strings.Contains(err.Error(), "unable to render operator") ||
-				strings.Contains(err.Error(), "literal contains invalid utf8") ||
-				strings.Contains(err.Error(), "literal contains null byte") ||
-				strings.Contains(err.Error(), "column name contains a double quote") ||
-				strings.Contains(err.Error(), "column name is empty") ||
-				strings.Contains(err.Error(), "the BETWEEN operator needs a two item list in the right hand side") {
-				return
-			}
-
-			t.Fatal(err)
+			// Ignore invalid expressions.
+			return
 		}
 
-		j, err := pg_query.ParseToJSON("SELECT * FROM test WHERE a = b AND (" + f + ")")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if strings.Contains(j, "CommentStmt") {
-			t.Fatal("CommentStmt found")
-		}
+		validateRender(t, e)
 	})
+}
+
+func validateRender(t *testing.T, e *expr.Expression) {
+	f, err := driver.NewPostgresDriver().Render(e)
+	if err != nil {
+		// Ignore errors that are expected.
+		if strings.Contains(err.Error(), "unable to render operator") ||
+			strings.Contains(err.Error(), "literal contains invalid utf8") ||
+			strings.Contains(err.Error(), "literal contains null byte") ||
+			strings.Contains(err.Error(), "column name contains a double quote") ||
+			strings.Contains(err.Error(), "column name is empty") ||
+			strings.Contains(err.Error(), "the BETWEEN operator needs a two item list in the right hand side") {
+			return
+		}
+
+		t.Fatal(err)
+	}
+
+	j, err := pg_query.ParseToJSON("SELECT * FROM test WHERE a = b AND (" + f + ")")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if strings.Contains(j, "CommentStmt") {
+		t.Fatal("CommentStmt found")
+	}
 }
