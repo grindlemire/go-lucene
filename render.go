f80d7ac963eff48f82c16bb9c8e554e3657ec100
grindlemire <grindlemire@gmail.com>
2026-04-18 15:51:56 -0400

    feat: add MySQL driver

--- render.go
+++ render.go
@@ -5,6 +5,7 @@ import "github.com/grindlemire/go-lucene/pkg/driver"
 var (
 	postgres = driver.NewPostgresDriver()
 	sqlite   = driver.NewSQLiteDriver()
+	mysql    = driver.NewMySQLDriver()
 )
 
 // ToPostgres is a wrapper that will render the lucene expression string as a postgres sql filter string.
@@ -48,4 +49,26 @@ func ToParameterizedSQLite(in string, opts ...Opt) (s string, params []any, err 
 	}
 
 	return sqlite.RenderParam(e)
+}
+
+// ToMySQL is a wrapper that will render the lucene expression string as a MySQL sql filter string.
+func ToMySQL(in string, opts ...Opt) (string, error) {
+	e, err := Parse(in, opts...)
+	if err != nil {
+		return "", err
+	}
+
+	return mysql.Render(e)
+}
+
+// ToParameterizedMySQL is a wrapper that will render the lucene expression string as a MySQL sql filter
+// string with parameters. The returned string will contain ? placeholders that can be passed directly to a
+// Query statement.
+func ToParameterizedMySQL(in string, opts ...Opt) (s string, params []any, err error) {
+	e, err := Parse(in, opts...)
+	if err != nil {
+		return "", nil, err
+	}
+
+	return mysql.RenderParam(e)
 }
